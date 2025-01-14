88943aebee6f5526388f3ab9571182d83c3e010c
VitaliiS <svjfox@gmail.com>
2025-01-14 10:52:43 +0200

    14

--- CarApp.ApplicationServices/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CarApp.ApplicationServices.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
